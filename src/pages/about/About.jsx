b2fed92e9c0f8e31ce9564fb87908538a92c59ad
Efeakpor <teefwesh0@gmail.com>
2022-09-02 20:30:18 +0100

    updated label

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./About.css";
 import Header from "../../components/header/Header";
 import ImageHeader from "../../components/imageHeader/ImageHeader";
-import DirectorSpeech from "../directorSpeech/DirectorSpeech";
 import Footer from "../footer/Footer";
 import RightSide from "../../components/rightSide/RightSide";
 import LeftSide from "../../components/leftSide/LeftSide";
